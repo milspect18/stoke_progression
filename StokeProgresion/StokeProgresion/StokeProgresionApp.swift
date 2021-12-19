1e1727c39c2f7735001aaaffc7be6155cb5666d1
Kyle Price <k.price.la@gmail.com>
2021-12-19 15:23:04 -0500

    Initial project creation

---
+++ StokeProgresion/StokeProgresion/StokeProgresionApp.swift
@@ -0,0 +1,17 @@
+//
+//  StokeProgresionApp.swift
+//  StokeProgresion
+//
+//  Created by Kyle Price on 12/19/21.
+//
+
+import SwiftUI
+
+@main
+struct StokeProgresionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
