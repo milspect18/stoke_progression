17fc7059ea7e5e5f7ccd5a0eace256c443fecff2
Kyle Price <k.price.la@gmail.com>
2021-12-19 16:22:40 -0500

    init and inject cata controller and moc

--- StokeProgresion/StokeProgresion/StokeProgresionApp.swift
+++ StokeProgresion/StokeProgresion/StokeProgresionApp.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 @main
 struct StokeProgresionApp: App {
+    @StateObject var dataController: DataController
+    
+    init() {
+        let controller = DataController()
+        _dataController = StateObject(wrappedValue: controller)
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+                .environmentObject(dataController)
         }
     }
 }
