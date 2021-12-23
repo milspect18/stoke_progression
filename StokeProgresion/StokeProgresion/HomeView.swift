2f21182c54c81692dea1710cbaf03a4e117362e7
Kyle Price <k.price.la@gmail.com>
2021-12-22 19:18:55 -0500

    implement home and projects view

---
+++ StokeProgresion/StokeProgresion/HomeView.swift
@@ -0,0 +1,31 @@
+//
+//  HomeView.swift
+//  StokeProgresion
+//
+//  Created by Kyle Price on 12/19/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @EnvironmentObject var dataController: DataController
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Button("Add Data") {
+                    dataController.deleteAll()
+                    try? dataController.createSampleData()
+                }
+            }
+            .navigationTitle("Home")
+        }
+        .navigationViewStyle(.stack)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ StokeProgresion/StokeProgresion/ProjectsView.swift
@@ -0,0 +1,50 @@
+//
+//  ProjectsView.swift
+//  StokeProgresion
+//
+//  Created by Kyle Price on 12/19/21.
+//
+
+import SwiftUI
+
+struct ProjectsView: View {
+    let showClosedProjects: Bool
+    let projects: FetchRequest<Project>
+    
+    init(showClosedProjects: Bool) {
+        self.showClosedProjects = showClosedProjects
+        self.projects = FetchRequest<Project>(
+            entity: Project.entity(),
+            sortDescriptors: [NSSortDescriptor(keyPath: \Project.creationDate, ascending: false)],
+            predicate: NSPredicate(format: "closed = %d", showClosedProjects),
+            animation: nil
+        )
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(projects.wrappedValue) { project in
+                    Section(header: Text(project.title ?? "")) {
+                        ForEach(project.items?.allObjects as? [Item] ?? []) { item in
+                            Text(item.title ?? "")
+                        }
+                    }
+                }
+            }
+            .listStyle(InsetGroupedListStyle())
+            .navigationTitle(showClosedProjects ? "Closed Projects" : "Open Projects")
+        }
+        .navigationViewStyle(.stack)
+    }
+}
+
+struct ProjectsView_Previews: PreviewProvider {
+    static var dataController = DataController.preview
+    
+    static var previews: some View {
+        ProjectsView(showClosedProjects: false)
+            .environment(\.managedObjectContext, dataController.container.viewContext)
+            .environmentObject(dataController)
+    }
+}
