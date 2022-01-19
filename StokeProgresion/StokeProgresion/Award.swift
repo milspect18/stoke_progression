96afb0c8d58c1471411c4da9dddb88f62030bc59
Kyle Price <k.price.la@gmail.com>
2022-01-19 17:48:26 -0500

    create award type and view

---
+++ StokeProgresion/StokeProgresion/Award.swift
@@ -0,0 +1,22 @@
+//
+//  Award.swift
+//  StokeProgresion
+//
+//  Created by Kyle Price on 1/19/22.
+//
+
+import Foundation
+
+struct Award: Decodable, Identifiable {
+    var id: String { name }
+    
+    let name: String
+    let description: String
+    let color: String
+    let criterion: String
+    let value: Int
+    let image: String
+    
+    static let allAwards = Bundle.main.decode([Award].self, from: "Awards.json")
+    static let example = allAwards[0]
+}
---
+++ StokeProgresion/StokeProgresion/AwardsView.swift
@@ -0,0 +1,22 @@
+//
+//  AwardsView.swift
+//  StokeProgresion
+//
+//  Created by Kyle Price on 1/19/22.
+//
+
+import SwiftUI
+
+struct AwardsView: View {
+    static let tag: String? = "Awards"
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AwardsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AwardsView()
+    }
+}
