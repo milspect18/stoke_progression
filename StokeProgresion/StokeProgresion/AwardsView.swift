fc838ea8f25320093b997456cc6f6f392effcb6c
Kyle Price <k.price.la@gmail.com>
2022-01-20 16:24:08 -0500

    build out awards view

--- StokeProgresion/StokeProgresion/AwardsView.swift
+++ StokeProgresion/StokeProgresion/AwardsView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct AwardsView: View {
+    @EnvironmentObject var dataController: DataController
+    
+    @State private var selectedAward = Award.example
+    @State private var showingAwardDetails = false
+    
     static let tag: String? = "Awards"
     
     var columns: [GridItem] {
@@ -20,19 +25,35 @@ struct AwardsView: View {
                 LazyVGrid(columns: columns) {
                     ForEach(Award.allAwards) { award in
                         Button {
-                            
+                            selectedAward = award
+                            showingAwardDetails = true
                         } label: {
                             Image(systemName: award.image)
                                 .resizable()
                                 .scaledToFit()
                                 .padding()
                                 .frame(width: 100, height: 100)
-                                .foregroundColor(.secondary.opacity(0.5))
+                                .foregroundColor(awardImageColor(for: award))
                         }
                     }
                 }
             }
+            .navigationTitle("Awards")
         }
+        .alert(alertTitle(for: selectedAward), isPresented: $showingAwardDetails) {
+            Button("Ok") { showingAwardDetails = false }
+        } message: {
+            Text(selectedAward.description)
+        }
+
+    }
+    
+    func awardImageColor(for award: Award) -> Color {
+        return dataController.hasEarned(award: award) ? Color(award.color) : .secondary.opacity(0.5)
+    }
+    
+    func alertTitle(for award: Award) -> String {
+        dataController.hasEarned(award: selectedAward) ? "Unlocked: \(award.name)" : "Locked"
     }
 }
 
